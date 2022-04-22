9628423f3f4e49a0d7b661b9d8a2de8306c59747
marzan131390 <marzandwmtec@gmail.com>
2022-04-22 15:34:41 +0600

    adding all tags is done

--- script/script.js
+++ script/script.js
@@ -2,33 +2,54 @@ let addBtn = document.querySelector(".add-btn");
 addBtn.addEventListener("click", displayIssue)
 //Display issue information 
 function displayIssue(event) {
-    //Getting Description Information
+    //new issue assign
 
+    // let issueID = document.createElement("p");
+    // issueID.classList.add("issue-id");
+    // issueID.innerHTML = `IssueId: <span> hello </span>`
+    // console.log(issueID);
 
-    issueInfo();
-    event.preventDefault();
-}
-
-//new issue assign
-function issueInfo() {
-    let issueID = document.createElement("p");
-    issueID.classList.add("issue-id");
-    issueID.innerHTML = `IssueId: <span> hello </span>`
-    console.log(issueID);
-
-
-
-    let descriptionInfo = document.getElementById("description-input").value;
-    console.log(descriptionInfo)
-    //Display issue into the response section.
+    //Taking info from description Input..
+    let descriptionInput = document.getElementById("description-input")
+    let descriptionInfo = descriptionInput.value;
+    //Taking severity value.
+    let severityValues = document.getElementById("severity-option").value;
+    //Taking Responsibility Value
+    let assignInput = document.getElementById("assign-input");
+    let assignInputValue = assignInput.value;
+    //Creating  Element for issue Description as h3 tag and display it to html..
+    let descriptionIssueElement = document.createElement("h3");
+    descriptionIssueElement.classList.add("issue-description");
+    descriptionIssueElement.innerText = descriptionInfo;
+    //creating  Element for option value as p tag  .
+    let optionPara = document.createElement("p");
+    optionPara.classList.add("option");
+    optionPara.innerHTML = severityValues;
+    //creating element for assign value as p tag
+    let assignPara = document.createElement("p");
+    assignPara.classList.add("assign");
+    assignPara.innerText = assignInputValue;
+    //Crating buttonDiv..
+    let buttonDiv = document.createElement("div");
+    buttonDiv.innerHTML = `<button class="close custom-btn">Close</button>
+    <button class="delete custom-btn">Delete</button>`
+    //Display description Info into the response section.
     let div = document.createElement("div");
     div.classList.add("new-response");
-    console.log(div);
-    div.appendChild(issueID);
+    // console.log(div);
+    //display Elements  to HTML
+    div.appendChild(descriptionIssueElement);
+    div.appendChild(optionPara);
+    div.appendChild(assignPara);
+    div.appendChild(buttonDiv);
     //Display Description Info to The issue Section.
     let allResponses = document.querySelector("#adding-responses");
     allResponses.appendChild(div);
 
-   
-   
-}
+    descriptionInput.value = "";
+    assignInput.value = "";
+ 
+    event.preventDefault();
+};
+
+
